07ab526dc52ffaa134db5f68492a785ac6008339
Paul Aldana <paul.aldana23@gmail.com>
2026-06-05 14:11:14 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True
+
+        # clean input string of spaces and non-alphanumeric characters and convert to all lowercase
+        cleaned_str = "".join(char for char in s if char.isalnum()).lower()
+        
+        return cleaned_str == cleaned_str[::-1]
+
+
+
+        # # two pointers -- won't work for odd number of characters in the array
+        # i = 0
+        # j = len(cleaned_str) - 1    # 0-indexed array
+
+        # while i != j:
+        #     if cleaned_str[i] == cleaned_str[j]:
+        #         i += 1
+        #         j -= 1
+        #     else: 
+        #         return False
+        # return True
