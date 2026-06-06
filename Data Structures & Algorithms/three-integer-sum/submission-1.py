4067a2b033fed7f65cb4a90fe9981095c1b2843a
Paul Aldana <paul.aldana23@gmail.com>
2026-06-05 22:41:20 -0700

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break       # all remaining numbers are positive, hence no possible solution
+
+            if i > 0 and a == nums[i - 1]:
+                continue    # Skip duplicate values for the first number.
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+
+        return res
+
+# 2 Pointers Method
+# Time: O(n^2), 
+# Space: O(m),  plus the space used by the sorting algorithm.
+# This excludes the space used for the output list.
+# If the output list is included, the space is O(m), which is O(n^2) in the worst case.
+#  - Where m is the number of unique triplets and n is the length of the given array.
