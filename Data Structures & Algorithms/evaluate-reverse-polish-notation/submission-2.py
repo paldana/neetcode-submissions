5d57ac31e2f5baa0f1492d3c26b1a0ad86fb5d7e
Paul Aldana <paul.aldana23@gmail.com>
2026-06-08 11:50:05 -0700

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        ops = ['+', '-', '*', '/']
+        for tkn in tokens:
+            # check every element of tokens and perform the op if an operator sign is seen
+            if tkn in ops:
+                # pop 2 numbers from stack and perform operation
+                newVal = 0
+                match tkn:
+                    case '+':
+                        op1, op2 = stack.pop(), stack.pop()
+                        newVal = op2 + op1
+                    case '-':
+                        op1, op2 = stack.pop(), stack.pop()
+                        newVal = op2 - op1
+                    case '*':
+                        op1, op2 = stack.pop(), stack.pop()
+                        newVal = op2 * op1
+                    case '/':
+                        op1, op2 = stack.pop(), stack.pop()
+                        newVal = int(float(op2) / op1)
+                stack.append(newVal)
+            else:
+                stack.append(int(tkn))
+        
+        return stack[0]
+
+# Stack Method
+# Time and Space Complexity: O(n)
