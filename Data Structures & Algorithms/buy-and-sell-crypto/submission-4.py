2cbab016574c0c88b716b2774b562a528b8ced18
Paul Aldana <paul.aldana23@gmail.com>
2026-06-09 13:57:13 -0700

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 2 pointers - Sliding window -> time: O(n^2); space: O(1)
+        l, r = 0, 1
+        maxProfit = 0
+        while r < len(prices):
+            # if we see a drop in price the next day, we move the l pointer to where r is
+            if prices[r] < prices[l]:
+                l = r
+            else:   # we see price increase, we keep moving the r pointer
+                maxProfit = max(maxProfit, prices[r] - prices[l])
+            r += 1
+        
+        return maxProfit
+
+            
