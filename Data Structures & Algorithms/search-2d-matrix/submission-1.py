c244a34b80bbc81c56daf6e4e5a42c791a56bb85
Paul Aldana <paul.aldana23@gmail.com>
2026-06-10 22:44:06 -0700

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # 2D-layer Binary Search - sorted arrays
+        l, r = 0, len(matrix[0]) - 1
+        top, bottom = 0, len(matrix) - 1
+        targetRow = 0
+        # locate the row that can possibly contain the target 
+        # then perform a binary search in the same row to find the target
+
+        while top <= bottom:
+            mid = (bottom + top)//2
+
+            if matrix[mid][0] > target:     # if val of the least element in the row < target
+                bottom = mid - 1            # move the bottom pointer to point to the smaller val rows
+            elif matrix[mid][len(matrix[0]) - 1] < target:  # highest val in row < target
+                top = mid + 1               # move top pointer to point to the higher val rows
+            else:
+                # we've found our rows
+                targetRow = mid
+                break
+        
+        # now perform binary search in the row
+        while l <= r:
+            m = (r + l) // 2
+
+            midVal = matrix[targetRow][m]
+            if target == midVal:
+                return True
+            
+            if midVal < target:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return False
