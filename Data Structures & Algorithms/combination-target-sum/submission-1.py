68f24745ae268e3ddabf357a44b687a18bbea6c6
Paul Aldana <paul.aldana23@gmail.com>
2026-06-23 09:57:37 -0700

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        # i is the pointer
+        # cur is the current list of combinations visited
+        # running total for the cur
+        def dfs(i, cur, total):
+            if total == target:
+                # since we're going to continue using the cur list, we'd want to save 
+                # a COPY of it to the res; if we don't whatever the end content of cur after 
+                # the recursions will be the same ones showing up saved to res.
+                # -- append in Python saves a reference to the original list object, 
+                #    hence why a copy of the same list is needed instead
+                res.append(cur.copy())  
+                # res.append(cur)       # will cause issues => same as newCombo = cur; res.append(newCombo)
+                # print(res)
+                return
+            if i >= len(nums) or total > target:
+                return
+
+
+            cur. append(nums[i])
+            dfs(i, cur, total + nums[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+        
+        dfs(0, [], 0)
+        return res
+
+# Backtracking Solution
+# Time & Space Complexity
+# Time complexity: O(2*(t/m))
+# Space complexity: O((t/m))
+# Where t is the given target and m is the minimum value in nums.
