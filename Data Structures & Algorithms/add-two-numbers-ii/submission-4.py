c43aed9b1186b7704756c8ddfe88eb836e1d7de2
Paul Aldana <paul.aldana23@gmail.com>
2026-06-30 14:48:45 -0700

    Add: add-two-numbers-ii - submission-4

---
+++ Data Structures & Algorithms/add-two-numbers-ii/submission-4.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        s1, s2 = [], []
+
+        while l1: 
+            s1.append(l1.val)
+            l1 = l1.next
+        
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+
+        head = None
+        carry = 0
+
+        while s1 or s2 or carry:
+            n1 = s1.pop() if s1 else 0
+            n2 = s2.pop() if s2 else 0
+            sum = n1 + n2 + carry
+            carry = sum // 10
+            node = ListNode(sum % 10)
+            node.next = head
+            head = node
+        
+        return head
+
+# Stack Method
+# Time & Space Complexity
+# Time complexity: O(m+n)
+# Space complexity: O(m+n)
+# Where m and n are the length l1 and l2, respectively
