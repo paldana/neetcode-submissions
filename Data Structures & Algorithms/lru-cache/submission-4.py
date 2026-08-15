a5828c3dec4cbb7f57f4efbd5818a6b624c0bf38
Paul Aldana <paul.aldana23@gmail.com>
2026-08-15 11:48:00 -0700

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,24 @@
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1 
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
+
+### Using Built-in Data Structure - collections.OrderedDict ###
+# Time complexity: O(1) for each put() and get() operation.
+# Space complexity: O(n)
