0b27f273fcdeabb00f3e76620add2fc6719ebd7c
Paul Aldana <paul.aldana23@gmail.com>
2026-08-18 11:35:10 -0700

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # DFS with Cycle Detection Solution
+        # create a prereq map - key: course, val: [prereqs]
+        preMap = {c: [] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        # cycle set = tracks the current DFS path for cycle deteciton
+        # visited set = tracks fully processed courses
+        cycle, visited = set(), set()
+        roadmap = []
+
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visited:
+                return True
+            
+            cycle.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            
+            cycle.remove(crs)
+            visited.add(crs)
+            roadmap.append(crs)
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+        return roadmap
+
+# DFS with Cycle Detection Solution
+# Time complexity: O(V+E)
+# Space complexity: O(V+E)
+# Where V is the number of courses and E is the number of prerequisites.
