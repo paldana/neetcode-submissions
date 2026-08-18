fe82ed2d2576e09cde41b4b2f6e8a32c039a2c59
Paul Aldana <paul.aldana23@gmail.com>
2026-08-18 09:45:56 -0700

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.py
@@ -0,0 +1,29 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        time = []
+        for i in intervals:
+            time.append((i.start, 1))
+            time.append((i.end, -1))
+            
+        # sorting using multiple criterias: 
+        # first by start time, then end time if start times are equal
+        time.sort(key=lambda x: (x[0], x[1]))
+        # print(time)
+
+        res = count = 0
+        for t in time:
+            count += t[1]
+            res = max(res, count)
+        return res
+
+## Greedy Solution
+# Time Complexity: O(n * log(n)) 
+# Space Complexity: O(n)
