397962ee117fd0c5ab2c39bdd5618e29aed8c46b
Paul Aldana <paul.aldana23@gmail.com>
2026-08-29 16:20:25 -0700

    Add: three-integer-sum - submission-14

---
+++ Data Structures & Algorithms/three-integer-sum/submission-14.py
@@ -0,0 +1,36 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+        # OR  
+        # count = {}
+        # for num in nums:
+        #   count[num] = count.get(num, 0) + 1
+
+        res = []
+        for i in range(len(nums)):
+            count[nums[i]] -= 1
+            if i and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] -= 1
+                if j - 1 > i and nums[j] == nums[j - 1]:
+                    continue
+                target = -(nums[i] + nums[j])
+                if count[target] > 0:
+                    res.append([nums[i], nums[j], target])
+
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] += 1
+        return res
+
+## Hashmap Solution -- probably more confusing compared to two pointers, so stick to that solution
+# Time: O(n^2) + O(n * log(n)) = O(n^2), 
+#       --> O(n * log(n)) for sorting
+# Space: O(n),  plus the space used by the sorting algorithm.
+# This excludes the space used for the output list.
+# O(n) is used for the frequency map.
+#  If the output list is included, the space is O(n+m), which is O(n^2) in the worst case.rray.
