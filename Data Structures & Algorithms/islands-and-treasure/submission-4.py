0cfff5e116ff6d9ada1b3ff20571f25b3abb7d3f
Paul Aldana <paul.aldana23@gmail.com>
2026-09-04 22:58:52 -0700

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        q = deque()
+        
+        # find the cells with treasures first and add them to queue
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    q.append([r, c])
+                    visit.add((r, c))
+
+        # helper function to add neighboring land cells to q
+        def addCell(r, c):
+            if (min(r, c) < 0 or r == ROWS or c == COLS or
+                (r, c) in visit or grid[r][c] == -1
+            ):
+                return
+            visit.add((r, c))
+            q.append([r, c])
+
+        # starting from the treasure cells, visit the neighboring cells
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                grid[r][c] = dist
+                addCell(r + 1, c)
+                addCell(r - 1, c)
+                addCell(r, c + 1)
+                addCell(r, c - 1)
+            dist += 1
