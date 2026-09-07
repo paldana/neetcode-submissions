4d6ec811bdbea75ba83402912afa5821e01f1947
Paul Aldana <paul.aldana23@gmail.com>
2026-09-06 23:14:18 -0700

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        visit = [False] * n
+
+        # retrieve all the adjacent nodes and save them in each node's respective array position 
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+        
+        # go through the neighboring nodes until the adj list is exhausted to determine all the connected node that will be formed as a single component
+        def dfs(i):
+            for neighbor in adj[i]:
+                if not visit[neighbor]:
+                    visit[neighbor] = True
+                    dfs(neighbor)
+
+
+        component = 0
+        for i in range(n):
+            if not visit[i]:
+                visit[i] = True
+                # go through the node's neighbors to determine all connected nodes that will form a single component
+                dfs(i)
+                # increase component after DFS completion
+                component += 1
+        
+        return component
