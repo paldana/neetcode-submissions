bffd13acbb360046ff108737e89817a7558976de
Paul Aldana <paul.aldana23@gmail.com>
2026-09-09 11:26:46 -0700

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        ## Brute Force - Recursion - first personal attempt - time limit exceeded at test 85/88 - not bad :D
+        # Time: O(2^n)
+        # Space: O(n)
+        # def dfs(steps, amount):
+        #     if steps >= len(cost):
+        #         return amount
+        #     amount += cost[steps]
+        #     return min(dfs(steps + 1, amount), dfs(steps + 2, amount))
+        # return min(dfs(0, 0), dfs(1, 0))
+
+        ## Recursion - NC solution - same issue when submitting, exceeding time limit at test 85/88 - meaning my answer above also works :D 
+        # Time: O(2^n)
+        # Space: O(n)
+        # def dfs(steps):
+        #     if steps >= len(cost):
+        #         return 0
+        #     return cost[steps] + min(dfs(steps + 1), dfs(steps + 2))
+        # return min(dfs(0), dfs(1))
+
+        ## DP - Memoized Approach
+        # Time: O(n)
+        # Space: O(n)
+        def dfs(steps, cache):
+            if steps >= len(cost):
+                return 0
+            
+            if steps in cache:
+                return cache[steps]
+            
+            cache[steps] = cost[steps] + min(dfs(steps + 1, cache), dfs(steps + 2, cache))
+
+            return cache[steps]
+
+        return min(dfs(0, {}), dfs(1, {}))
