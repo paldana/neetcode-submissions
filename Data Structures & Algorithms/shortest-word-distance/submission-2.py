b306628bff4237e8c2331299463c3ba007ed95e6
Paul Aldana <paul.aldana23@gmail.com>
2026-09-09 16:18:29 -0700

    Add: shortest-word-distance - submission-2

---
+++ Data Structures & Algorithms/shortest-word-distance/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        
+        indexes = defaultdict(list)
+
+        for index, word in enumerate(wordsDict):
+            indexes[word].append(index)
+
+        min_dist = float('inf')
+        for i in indexes[word1]:
+            for j in indexes[word2]:
+                min_dist = min(abs(i - j), min_dist)
+
+        return min_dist
