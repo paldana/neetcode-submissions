d2394a8f492e9d435872b76aee4645bc7632849d
Paul Aldana <paul.aldana23@gmail.com>
2026-09-10 13:59:06 -0700

    Add: shortest-word-distance-ii - submission-2

---
+++ Data Structures & Algorithms/shortest-word-distance-ii/submission-2.py
@@ -0,0 +1,28 @@
+class WordDistance:
+
+    def __init__(self, words: List[str]):
+        self.locations = defaultdict(list)
+
+        # Prepare a mapping from a word to all it's locations (indices).
+        for i, w in enumerate(words):
+            self.locations[w].append(i)
+
+    def shortest(self, word1: str, word2: str) -> int:
+        min_diff = float("inf")
+        
+        for i1 in self.locations[word1]:
+            for i2 in self.locations[word2]:
+                min_diff = min(min_diff, abs(i1 - i2))
+
+        return min_diff
+
+"""
+Hashmap Solution
+Time complexity: O(n + a * b)
+Space complexity: O(n)
+
+where
+    n is the number of elements in the input
+    a is the number of occurrences of word1
+    b is the number of occurrences of word2
+"""
