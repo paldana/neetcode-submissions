3a8d85b855e12c5436231a863a96b745c81b2899
Paul Aldana <paul.aldana23@gmail.com>
2026-09-10 09:20:29 -0700

    Add: brace-expansion - submission-1

---
+++ Data Structures & Algorithms/brace-expansion/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def expand(self, s: str) -> List[str]:
+        # Iterative approach - we'll create the string as we go through every options in string, s
+
+        # 1 - create an array to store expanding words - will be the output
+        expandedWords = [""]
+
+        i = 0   
+        # go through every character of s
+        while i < len(s):
+            # create temp array that will store the new words we'll create
+            options = []
+
+            # extract the options we have from the s starting from i
+            if s[i] != "{":
+                options.append(s[i])
+            else:
+                while s[i] != "}":
+                    # check if character
+                    if 'a' <= s[i] <= 'z':
+                        options.append(s[i])
+                    i += 1
+                # at this point, i is pointing to an '}'
+
+            i += 1          # increment to point to the next unvisited option in s
+            newWords = []
+            for word in expandedWords:
+                for c in options:
+                    newWords.append(word + c)
+            
+            # update the expandedWords to contain the newWords with the newly appended characters from options
+            expandedWords = newWords
+        
+        return expandedWords
+
+
