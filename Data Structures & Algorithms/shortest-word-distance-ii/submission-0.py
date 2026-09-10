ae50232045d960b98c6fb65867b65f1252ce71b7
Paul Aldana <paul.aldana23@gmail.com>
2026-09-10 11:37:53 -0700

    Add: shortest-word-distance-ii - submission-0

---
+++ Data Structures & Algorithms/shortest-word-distance-ii/submission-0.py
@@ -0,0 +1,22 @@
+class WordDistance:
+    def __init__(self, wordsDict: List[str]):
+        self.wordArr = wordsDict
+
+    def shortest(self, word1: str, word2: str) -> int:
+        # two-pointer approach
+        i1, i2 = -1, -1
+        minDist = len(self.wordArr) + 1  # arbitrary large number
+        for idx, word in enumerate(self.wordArr):
+            if word == word1:
+                i1 = idx
+            elif word == word2:
+                i2 = idx
+
+            if i1 != -1 and i2 != -1:
+                minDist = min(minDist, abs(i1 - i2))
+        return minDist
+
+
+# Your WordDistance object will be instantiated and called as such:
+# obj = WordDistance(wordsDict)
+# param_1 = obj.shortest(word1,word2)
