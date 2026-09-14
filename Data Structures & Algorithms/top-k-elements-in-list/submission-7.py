9d4f931417ac3d18febe14da26038897b8c9a1e7
Paul Aldana <paul.aldana23@gmail.com>
2026-09-13 21:59:17 -0700

    Add: top-k-elements-in-list - submission-7

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-7.py
@@ -0,0 +1,54 @@
+from collections import Counter
+from heapq import heappush, heappop
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ## Sorting Approach - Time: O(n log(n)), Space: O(n)
+        # freqMap = Counter(nums)
+
+        # freq = []
+        # for num, cnt in freqMap.items():
+        #     freq.append((cnt, num))
+        # freq.sort(reverse=True)
+    
+        # res = []
+        # for i in range(k):
+        #     res.append(freq[i][1])
+        # return res
+
+        ## MinHeap Approach - Time: O(n*log(k)), Space: O(n)
+        # maxHeap = []
+        # freqMap = {}
+        # for num in nums:
+        #     freqMap[num] = freqMap.get(num, 0) + 1
+        
+        # for num, cnt in freqMap.items():
+        #     heappush(maxHeap, (-1 * cnt, num))
+
+        # res = []
+        # for _ in range(k):
+        #     res.append(heappop(maxHeap)[1])
+        # return res
+
+
+        ## Bucket Sort Approach - Optimal
+        freq = {}
+        numCount = [[] for i in range(len(nums)+1)]
+
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        
+        for num, cnt in freq.items():
+            numCount[cnt].append(num)
+        
+        res = []
+        for i in range(len(numCount)-1, -1, -1):
+            for num in numCount[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+            
+        
+        
+            
