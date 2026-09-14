751ff640ed9fdf02f9d2e802895f4486891b00d5
Paul Aldana <paul.aldana23@gmail.com>
2026-09-13 22:29:04 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]: 
+        # anaMap = defaultdict(list)     # key: char signature, val: list of anagram words 
+        # for s in strs:
+        #     charSignature = [0] * 26
+        #     for c in s:
+        #         charSignature[ord(c) - ord('a')] += 1
+        #     anaMap[tuple(charSignature)].append(s)
+        
+        # # res = []
+        # # for k, v in anaMap.items():
+        # #     res.append(v)
+        # # return res
+        # ## shorter version
+        # return list(anaMap.values())
+
+        ## Hash Map - 2
+        anaMap = defaultdict(list)
+        for word in strs:
+            anaMap["".join(sorted(list(word)))].append(word)
+        return list(anaMap.values())
