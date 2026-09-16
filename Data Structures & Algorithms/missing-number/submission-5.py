2d42e905fe2cd050d45341a16885c53f9f21f9fd
Paul Aldana <paul.aldana23@gmail.com>
2026-09-16 15:19:01 -0700

    Add: missing-number - submission-5

---
+++ Data Structures & Algorithms/missing-number/submission-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        xorr = n            # important to initialize with n since we're expecting values from 0 to n
+        for i in range(n):
+            xorr ^= i ^ nums[i]     # after going through the list, all values from 0 to n-1 will be cancelled out by XOR ^ if it's in the list
+        return xorr         # will return the missing number 
+
+## Bit Manipulation - Time and Space: O(1)
