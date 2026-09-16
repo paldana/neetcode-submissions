e61cfbd790863dc335f3e9904481ac427ad7cc26
Paul Aldana <paul.aldana23@gmail.com>
2026-09-16 10:55:19 -0700

    Add: number-of-one-bits - submission-3

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        ## Bit Mask I
+        # Time: O(log n), Space: O(1)
+        count = 0
+        while n > 0:
+            if n & 1:
+                count +=1 
+            n >>= 1
+        return count
+        
+        ## Bit Mask I - better Time Complexity??
+        # Time: O(1), Space: O(1)
+        ## How is this better when it's pretty much the same?
+        # res = 0
+        # while n:
+        #     res += 1 if n & 1 else 0
+        #     n >>= 1
+        # return res
+
+        ## Bit Mask II - better 
+        # Time: O(1), Space: O(1)
+        # count = 0
+        # for i in range(32):
+        #     if (1 << i) & n:
+        #         count += 1
+        # return count
+
