bd329e4d6791d8c65d201ea9eb170e863f00628d
Paul Aldana <paul.aldana23@gmail.com>
2026-09-23 13:43:21 -0700

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.py
@@ -0,0 +1,32 @@
+## Dynamic Programming Top-Down Approach - Memoization x Recursion 
+# Time: O(n * m)
+# Space: O(n * m)
+# where m and n are lengths of text1 and text2, respectively
+# There are two sources of space usage:
+#     Memoization dictionary: up to n × m entries
+#     Recursion call stack: at most O(n + m) deep, since every call advances i, j, or both
+#     Hence, we get O(n × m) + O(n + m) = O(n × m)
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = {}   # key: indices (i, j), value: LCS at cell(i,j)
+
+        # find the LCS at cell (i,j)
+        def dfs(i, j):
+            # base case - if we've reached the end of either string
+            if i == len(text1) or j == len(text2):
+                return 0
+            
+            if (i,j) in memo:
+                return memo[(i,j)]
+
+            # if current char matches
+            if text1[i] == text2[j]:
+                # add 1 for LCS and recurse to the next char for both strings
+                memo[(i,j)] = 1 + dfs(i + 1, j + 1)
+            else:
+                # recurse by skipping text1[i] or text2[j] and return the maximum
+                memo[(i,j)] = max(dfs(i + 1, j), dfs(i, j + 1))
+            return memo[(i,j)]
+
+        return dfs(0, 0)
